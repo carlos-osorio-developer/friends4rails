91595cff62ab78f670bd891a541b4640106ad8d6
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-09 18:42:35 -0500

    Add controller for the friendship

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,37 @@
+class FriendshipsController < ApplicationController
+
+  def index
+    @friendships = Friendship.all
+  end
+
+  def show
+    @friendship = Friendship.find(params[:id])
+  end
+
+  def new
+    @friendship = Friendship.new
+  end
+
+  def create
+    @friendship = Friendship.new
+    @friendship.user_id = params[:user_id]
+    @friendship.friend_id = params[:friend_id]
+
+    if @friendship.save
+      render json: @friendship, status: :created, location: @friendship
+    else
+      render json: @friendship.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    @friendship.destroy
+  end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:user_id, :friend_id)
+  end
+end
