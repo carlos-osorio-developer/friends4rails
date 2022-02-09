b7bc8373428604740082c1c6a5919bd2132fa8a7
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-09 14:16:55 -0500

    Configure factories with Faker gem

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    name { Faker::Name.name }
+    bithdate { Faker::Date.birthday(18, 65) }
   end
 end
