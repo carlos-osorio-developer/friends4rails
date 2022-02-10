3821274cc8fc45838b5a749d7b60b225f46bf5cc
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-10 16:11:29 -0500

    Add Comment model with associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
-
-  validates :email, presence: true, uniqueness: true
-
+  has_many :comments, dependent: :destroy  
   has_many :friendships
   has_many :friends, through: :friendships
+
+  validates :email, presence: true, uniqueness: true
 
   scope :available, lambda { |user|  
     where.not(id: user.friends.ids)
